72a8f8f91f79deba589741e5803b0134bc96b8ef
J Coggshall <hedge@runbox.com>
2017-02-23 23:31:24 -0800

    Added clean attention scaffolding

---
+++ CleanAttentionMech.py
@@ -0,0 +1,23 @@
+# Attention Mechanism From Scratch
+
+# Input: Encoder Hidden States
+# Output: Weights For Each Encoder State representing 
+
+# Question: How is it differentiable?
+# Question: How does it work together during the training loop? (specifically with TF)
+# How does it backprop? (specifically with TF)
+# How does it fit in the context of the overall model?
+
+def generateAttentionProbs(encoderstates):
+
+
+
+
+	# LSTM cell??
+
+	# run softmax
+
+	# return weighted probs associated with each encoder hidden state?
+
+	return 
+
